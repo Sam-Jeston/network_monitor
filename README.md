fb4433b27c83d4ed7c5ee3663ab95ff36dea624c
Sam Jeston <sam.jeston@gmail.com>
2017-05-14 19:16:30 +1000

    Change to pnet crate

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Network Monitor
 
 A command line tool written in Rust.
-
-Inspired by https://github.com/k0pernicus/packetSniffer
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 extern crate argparse;
-extern crate pcap;
+extern crate pnet;
 
 use argparse::{ArgumentParser, Store, StoreTrue};
-use pcap::{Capture, Device, Stat};
 
-// Print available devices
-fn print_available_devices<'a> (vec_devices : &'a Vec<Device>) {
-    println!("Available Devices: ", );
-    for device in vec_devices {
-        println!("\t Device {:?} : {:?}", device.name, device.desc)
+use pnet::datalink::{self, NetworkInterface};
+use pnet::datalink::Channel::Ethernet;
+use pnet::packet::{Packet, MutablePacket, PacketSize};
+
+// Print available network interfaces
+fn print_available_interfaces () {
+    println!("Available Network interfaces: ", );
+    let interfaces = datalink::interfaces();
+    for interface in interfaces {
+        println!("\t Interface - {:?}", interface.name)
     }
 }
 
 // Get request device
-fn get_requested_device<'a> (requested_device_s : &str, requested_device : &'a mut Device, vec_devices : &'a Vec<Device>) {
-    for device in vec_devices {
-        if &*device.name == requested_device_s {
-            requested_device.name = device.name.clone();
-            requested_device.desc = device.desc.clone();
-            println!("-{} device captured", requested_device_s);
-        }
-    }
+fn get_requested_interface (requested_interface : &str) -> pnet::datalink::NetworkInterface {
+    let interface_names_match = |iface: &NetworkInterface| iface.name == requested_interface;
+    datalink::interfaces()
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap()
 }
 
 fn main() {
-    let mut requested_device : Device = Device::lookup().unwrap();
-
     let mut print_devices : bool = false;
-    let mut requested_device_s : String = "wlp2s0".to_string();
+    let mut requested_interface_s : String = "wlp2s0".to_string();
     let mut verbose : bool = false;
 
     // What is the purpose of this block?
@@ -36,46 +37,40 @@ fn main() {
         argparse.set_description("Lets look at some packets");
         argparse.refer(&mut print_devices)
             .add_option(&["-p", "--print_devices"], StoreTrue, "Print devices found");
-        argparse.refer(&mut requested_device_s)
+        argparse.refer(&mut requested_interface_s)
             .add_option(&["-d", "--device"], Store, "Request a device");
         argparse.refer(&mut verbose)
             .add_option(&["-v", "--verbose"], Store, "Be verbose");
         argparse.parse_args_or_exit();
     }
 
-    let devices = Device::list();
+    println!("requested_device : {}", requested_interface_s);
 
-    println!("requested_device : {}", requested_device_s);
+    if print_devices {
+        print_available_interfaces();
+        std::process::exit(0);
+    }
 
-    match devices {
-        Ok(vec_devices) => {
-            if print_devices {
-                print_available_devices(&vec_devices);
-                std::process::exit(0);
+    let interface = get_requested_interface(&requested_interface_s);
+
+    let (mut _tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("An error occurred when creating a datalink channel: {}", e)
+    };
+
+    let mut iter = rx.iter();
+    loop {
+        match iter.next() {
+            Ok(packet) => {
+                let packet_size = packet.packet_size();
+                let source = packet.get_source();
+                println!("Packet size: {}", packet_size);
+                println!("Packet source: {}", source);
             }
-            get_requested_device(&requested_device_s, &mut requested_device, &vec_devices);
+            Err(_) => {
+                panic!("An error occured")
+            }
         }
-        Err(_) => {
-            println!("No device found");
-            std::process::exit(1);
-        }
-    }
-
-    if requested_device_s != requested_device.name {
-        std::process::exit(1);
-    }
-
-    println!("Default device, in case you are lost!! : {:?}", Device::lookup().unwrap().name);
-
-    let mut cap = Capture::from_device(requested_device).unwrap().open().unwrap();
-
-    // while let Ok(packet) = cap.next() {
-    //     println!("received packet! {:?}", packet);
-    // }
-
-    // Relatively sure this is the number of packets received. Maybe we count bytes from packet.data
-    // And channel that somewhere in a clever way
-    while let Ok(stat) = cap.stats() {
-        println!("..... {:?}", stat.received);
     }
 }
